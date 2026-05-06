e35fc0930f53a6f6ce7376d9bfe0771e69e29757
unknown <gawares222@gmail.com>
2026-05-06 14:35:01 +0530

    fix history URL and add CORS

--- backend/main.py
+++ backend/main.py
@@ -430,3 +430,13 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+    
+    
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
